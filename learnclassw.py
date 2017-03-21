0108b248ba1d77315dc476549c7c7ce8e430e7f4
ShanYuQH <15281834450@163.com>
2017-03-21 20:51:39 +0800

    Update learnclassw.py

--- learnclassw.py
+++ learnclassw.py
@@ -5,6 +5,5 @@ from enum import Enum
 
 Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
 
-
 for name, member in Month.__members__.items():
     print(name, '=>', member, ',', member.value)
